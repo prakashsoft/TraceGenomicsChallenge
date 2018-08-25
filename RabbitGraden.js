83938b720ef59f57c038d48d20f8a76c58c5ed4f
prakashsoft <prakash.bahadur@gmail.com>
2018-08-24 17:39:12 -0700

    added some comment

--- RabbitGraden.js
+++ RabbitGraden.js
@@ -2,7 +2,7 @@
 // Input: An N x M matrix of a garden. Each cell contains a positive integer representing the number of carrots in that part of the garden.
 // Output: The number of carrots Bunny eats before falling asleep.
 
-
+// Define Variables
 const findtrueNeighboringCells = (row, column, garden1) => {
   const trueNeighboringCells = {}
 
@@ -10,6 +10,7 @@ const findtrueNeighboringCells = (row, column, garden1) => {
   const MoveDown = row !== garden1.length - 1 && garden1[row + 1][column] > 0
   const MoveLeft = column !== 0 && garden1[row][column - 1] > 0
   const MoveRight = column !== garden1[0].length - 1 && garden1[row][column + 1] > 0
+// Apply all the conditions for matrix
 
   if (MoveUp) {
     trueNeighboringCells['up'] = {
@@ -41,12 +42,12 @@ const findtrueNeighboringCells = (row, column, garden1) => {
   }
   return trueNeighboringCells
 }
-
+// If conditions met find out how many carrots eat by ...
 const numberofCarrotsToEat = garden1 => {
   const width = garden1[0].length
   const height = garden1.length
+// to find out how many cell close to middle of the garden 1 with maximum number of carrot.
 
-  // TODO: determine cell close to middle of garden with max carrot count
   let row = Math.floor(width / 2)
   let column = Math.floor(height / 2)
 
@@ -60,7 +61,7 @@ const numberofCarrotsToEat = garden1 => {
       'direction': ''
     }
 
-    // Determine which neighboring cell has the largest amount of carrots
+    // To find out which neighbor cell has highest number of carrot
     for (direction in neighboringCells) {
       if (neighboringCells[direction]['cellValue'] > neighboringCellWithMaxValue['cellValue']) {
         neighboringCellWithMaxValue['cellValue'] = neighboringCells[direction]['cellValue']
@@ -68,14 +69,14 @@ const numberofCarrotsToEat = garden1 => {
       }
     }
 
-    garden1[row][column] = -1 // mark current cell as visited
+    garden1[row][column] = -1 // current cell visited by Bunny
 
     if (neighboringCellWithMaxValue['cellValue'] === 0) {
-      return carrotsToEat // done
+      return carrotsToEat
     } else {
       carrotsToEat += neighboringCellWithMaxValue['cellValue']
 
-      // move
+      // move again
       row = neighboringCells[neighboringCellWithMaxValue['direction']]['row']
       column = neighboringCells[neighboringCellWithMaxValue['direction']]['column']
     }
